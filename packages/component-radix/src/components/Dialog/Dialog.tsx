f73de61cad0c2b6dccdadf137665bc2aea632d80
LEECHANHYUNG <max.lee@goorm.io>
2025-01-28 15:11:56 +0900

    feat: 프로젝트 구성

---
+++ packages/component-radix/src/components/Dialog/Dialog.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import * as Dialog from "@radix-ui/react-dialog";
+import styles from "./styles.module.css";
+
+const DialogDemo = () => (
+  <Dialog.Root>
+    <Dialog.Trigger asChild>
+      <button className={`${styles.Button} violet`}>Edit profile</button>
+    </Dialog.Trigger>
+    <Dialog.Portal>
+      <Dialog.Overlay className={styles.Overlay} />
+      <Dialog.Content className={styles.Content}>
+        <Dialog.Title className={styles.Title}>Edit profile</Dialog.Title>
+        <Dialog.Description className={styles.Description}>
+          Make changes to your profile here. Click save when you're done.
+        </Dialog.Description>
+        <fieldset className={styles.Fieldset}>
+          <label className={styles.Label} htmlFor="name">
+            Name
+          </label>
+          <input
+            className={styles.Input}
+            id="name"
+            defaultValue="Pedro Duarte"
+          />
+        </fieldset>
+        <fieldset className={styles.Fieldset}>
+          <label className={styles.Label} htmlFor="username">
+            Username
+          </label>
+          <input
+            className={styles.Input}
+            id="username"
+            defaultValue="@peduarte"
+          />
+        </fieldset>
+        <div
+          style={{ display: "flex", marginTop: 25, justifyContent: "flex-end" }}
+        >
+          <Dialog.Close asChild>
+            <button className={`${styles.Button} green`}>Save changes</button>
+          </Dialog.Close>
+        </div>
+        <Dialog.Close asChild>
+          <button className={styles.IconButton} aria-label="Close">
+            x
+          </button>
+        </Dialog.Close>
+      </Dialog.Content>
+    </Dialog.Portal>
+  </Dialog.Root>
+);
+
+export default DialogDemo;
